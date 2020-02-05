d12f11c3f0eaa21b380a59787b4c06e3d0abe71e
rajivphanju <razibphanju@gmail.com>
2020-02-05 10:35:07 +0545

    update and delete api

--- routes/CompanyRegister.js
+++ routes/CompanyRegister.js
@@ -25,4 +25,47 @@ router.post("/companyRegister",(req,res)=>{
             });
         });
 });
+
+router.get('/allCompanies', function (req, res) {
+    Company.find().then(function (companies) {
+        res.send(companies);
+    }).catch(function (e) {
+        res.send(e);
+    });
+});
+
+router.get('/fetchSingleCompany/:id', function (req, res) {
+    var companyId = req.params.id.toString();
+
+    Company.find({
+        _id: companyId
+    }).then(function (company) {
+        res.send(company);
+
+    }).catch(function (e) {
+        res.send(e);
+    });
+
+});
+
+router.put('/updateCompany/:id', function (req, res) {
+    CompanyId = req.params.id.toString();
+
+    Company.findByIdAndUpdate(CompanyId, req.body, {
+        new: true
+    }).then(function (company) {
+        res.send(company);
+    }).catch(function (e) {
+        res.send(e);
+    });
+});
+router.delete('/deleteCompany/:id', function (req, res) {
+    Company.findByIdAndDelete(req.params.id).then(function (company) {
+        res.json({
+            message: "Deleted Successfully"
+        })
+    }).catch(function (e) {
+        res.send(e);
+    });
+});
 module.exports = router;
